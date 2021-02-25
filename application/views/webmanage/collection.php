ad8ab0fe8dc00cbb45d22d5461dfb71a005b5cf1
jee <s6202041520016@email.kmutnb.ac.th>
2021-02-26 00:52:38 +0700

    a

--- application/views/webmanage/collection.php
+++ application/views/webmanage/collection.php
@@ -320,7 +320,10 @@ var obj = JSON.parse(data);
 for (let index = 0; index < obj.length; index++) {
 //   (13.614289702759658, 100.62068939208984)
     var latLng = "("+obj[index].Lat+","+obj[index].Lot+")";
-  
+    
+  console.log(obj[index].Lat);
+  console.log(obj[index].Lot);
+
   var marker = new google.maps.Marker({
     position: {lat : obj[index].Lat ,lng : obj[index].Lot },
     map: map,
