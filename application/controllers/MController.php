62b7b5a35159682d27149d364b6a5534d638110d
Tony <panjantip@gmail.com>
2021-02-27 19:01:41 +0700

    pretty code

--- application/controllers/MController.php
+++ application/controllers/MController.php
@@ -1,41 +1,26 @@
 <?php 
-/**
- * 
- */
+
 class MController extends CI_Controller
 {
   
   function __construct()
   {
-        parent::__construct();  
-     
+    parent::__construct();
   }
-    
 
-      public function index(){
-                     
-                    $this->load->view('webmanage/header' ); 
-                    $this->load->view('webmanage/index' ); 
-                    $this->load->view('webmanage/footer' ); 
-     }
+  public function index()
+  {
+    $this->load->view('webmanage/header');
+    $this->load->view('webmanage/index');
+    $this->load->view('webmanage/footer');
+  }
 
-     public function collection(){
-                     
-                    $this->load->view('webmanage/header' ); 
-                    $this->load->view('webmanage/collection' ); 
-                    $this->load->view('webmanage/footer' ); 
-     }
-
-
-
-
-     
- 
-
+  public function collection()
+  {
+    $this->load->view('webmanage/header');
+    $this->load->view('webmanage/collection');
+    $this->load->view('webmanage/footer');
+  }
 }
   
 ?>
-
-
-
-
--- application/controllers/Qrcontroller.php
+++ application/controllers/Qrcontroller.php
@@ -1,67 +1,43 @@
-<?php 
-/**
- * 
- */
+<?php
+
 class Qrcontroller extends CI_Controller
 {
   
   function __construct()
   {
-        parent::__construct();
-        $this->load->model("QRModel"); 
-       
-     
+    parent::__construct();
+    $this->load->model("QRModel"); 
   }
-    
 
-      public function index(){
-                     
-                    $this->load->view('webqrcode/header' ); 
-                    $this->load->view('webqrcode/index' ); 
-                    $this->load->view('webqrcode/footer' ); 
-     }
-     public function  testdb()
-     {
+  public function index()
+  {
+    $this->load->view('webqrcode/header');
+    $this->load->view('webqrcode/index');
+    $this->load->view('webqrcode/footer');
+  }
 
-      echo $this->QRModel->testsql();
+  public function testdb()
+  {
+    echo $this->QRModel->testsql();
+  }
 
-     }
+  public function insertlocation()
+  {
+    $Latitude = $_POST["Latitude"];
+    $Longitude = $_POST["Longitude"];
+    $Img = $_POST["Img"];
+    $Textpf = $_POST["Textpf"];
+    echo $this->QRModel->insertlocation($Latitude,$Longitude,$Img,$Textpf);
+  }
 
+  public function getlocation()
+  {
+    echo json_encode($this->QRModel->getlocation());
+  }
 
-     public function  insertlocation()
-     {
-
-       //print_r($_POST);
-       $Latitude = $_POST["Latitude"];
-       $Longitude = $_POST["Longitude"];
-       $Img = $_POST["Img"];
-       $Textpf = $_POST["Textpf"];
-       echo $this->QRModel->insertlocation($Latitude,$Longitude,$Img,$Textpf);
-
-       
-     }
-     
-
-     public function getlocation()
-      {
-
-            echo json_encode($this->QRModel->getlocation());
-
-      }
-
-      public function GetDatabyID()
-      {
-        echo json_encode($this->QRModel->GetDatabyID($_POST['id']));
-      }
-
-
+  public function GetDatabyID()
+  {
+    echo json_encode($this->QRModel->GetDatabyID($_POST['id']));
+  }
 }
- 
-
- 
-
 ?>
-
-
-
-
