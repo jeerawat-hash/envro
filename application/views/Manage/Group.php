e0720cd27b33c2602fb8154e52baddeff48acf17
Tony <panjantip@gmail.com>
2021-03-08 15:19:57 +0700

    Draft views - edit #2 Include/Head

--- application/views/Manage/Group.php
+++ application/views/Manage/Group.php
@@ -1,28 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<title><?=$this->config->item('project').' | '.$FuncName;?></title>
-	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-	<link rel="icon" href="<?=base_url('ui/assets/img/icon.ico')?>" type="image/x-icon"/>
-	
-	<!-- Fonts and icons -->
-	<script src="<?=base_url('ui/assets/js/plugin/webfont/webfont.min.js')?>"></script>
-	<script>
-		WebFont.load({
-			google: {"families":["Lato:300,400,700,900"]},
-			custom: {"families":["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands", "simple-line-icons"], urls: ['<?=base_url('ui/assets/css/fonts.min.css')?>']},
-			active: function() {
-				sessionStorage.fonts = true;
-			}
-		});
-	</script>
-
-	<!-- CSS Files -->
-	<link rel="stylesheet" href="<?=base_url('ui/assets/css/bootstrap.min.css')?>">
-	<link rel="stylesheet" href="<?=base_url('ui/assets/css/atlantis.min.css')?>">
-	<!-- CSS Just for demo purpose, don't include it in your project -->
-	<link rel="stylesheet" href="<?=base_url('ui/assets/css/demo.css')?>">
+<?php $this->view('Include/Head');?>
 </head>
 <body>
 	<div class="wrapper">
